e24990157331aeaff42df16ee223b4654a3303c5
roman <skyfoxwork@gmail.com>
2024-12-08 14:12:02 +0200

    Solution-py-api-views v1.1

--- cinema/urls.py
+++ cinema/urls.py
@@ -27,8 +27,6 @@ router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    # path("movies/", movie_list, name="movie-list"),
-    # path("movies/<int:pk>/", movie_detail, name="movie-detail"),
     path("genres/", GenreList.as_view(), name="gener-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="gener-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
